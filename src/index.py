3cb1b7ae9422d7be9e40f2be6af7e940d690f721
Nirajan Thapa <thapanirajan789@gmail.com>
2026-02-22 10:19:28 +0545

    update: Update frontend allowed origins for CORS

--- src/index.py
+++ src/index.py
@@ -34,7 +34,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://resume-ez-frontend-z32r.vercel.app"
+        "https://resume-ez-frontend-z32r.vercel.app",
+        "https://resume-ez-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
